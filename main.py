b45a4df1aaf7aa4c4a5826dd746d70faf83b8174
Vlad <vladsach@gmail.com>
2020-03-26 13:26:48 +0200

    small fix

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ class EndList:
 
     def __init__(self, list):
         self.list = list
+        self.longest_word = list.index(max(list, key=len))  # находит максимальное
+        self.shortest_word = list.index(min(list, key=len))  # находит минимальное
 
-    def long_word(self):  # находит максимальное
-        longest_word = list.index(max(list, key=len))
-
-    def short_word(self):  # находит минимальное
-        shortest_word = list.index(min(list, key=len))
-
-    def change_pos(self, longest_word, shortest_word):  # меняет местами
-        list[shortest_word], list[longest_word] = list[longest_word], list[shortest_word]
+    def change_pos(self):  # меняет местами
+        self.list[self.shortest_word], self.list[self.longest_word] = self.list[self.longest_word], self.list[self.shortest_word]
 
     def __del__(self):
         print('список потерт')
@@ -36,6 +32,12 @@ print("=================================")
 
 list_first = Line(input('Введите строку: ')).splitting()  # создает массив со словами
 print('Начальный список: ', list_first)
-list_last = EndList(list_first)
-list_last.change_pos(list_last.long_word(), list_last.short_word())
+list_last = EndList(list_first).change_pos()
 print("Конечный список: ", list_last)
+
+# Вариант без ООП
+"""s = input().split()
+i = s.index(min(s, key=len))
+j = s.index(max(s, key=len))
+s[i], s[j] = s[j], s[i]
+print(' '.join(s))"""
