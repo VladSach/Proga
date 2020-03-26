5ff651ddb04c43a18fc909bd1dfc1ebc958afc45
Vlad <vladsach@gmail.com>
2020-03-26 12:38:18 +0200

    linked list

---
+++ main.py
@@ -0,0 +1,45 @@
+class ListNode:
+    def __init__(self, key, prev=None, _next=None):
+        self.key = key
+        self.next = _next
+        self.prev = prev
+
+        def __del__(self):
+            print('узел удален')
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, key):
+        new = ListNode(key, _next=self.head)
+        if self.head:
+            self.head.prev = new
+        self.head = new
+
+    def search(self, key):
+        current = self.head
+        while current and current.key != key:
+            current = current.next
+        return current
+
+    def delete(self, node):
+        if node.prev:
+            node.prev.next = node.next
+        else:
+            self.head = node.next
+        if node.next:
+            node.next.prev = node.prev
+
+    def __del__(self):
+        print('список потерт')
+
+
+print("Работу выполнил Саченко Владислав ИП-91 на языке программировании Python")
+print("Вариант №22")
+print("Заданий рядок слів, які відокремлюються одне від одного будь-якою кількістю пробілів.")
+print("Побудувати список відповідних слів. Поміняти місцями найдовше та найкоротше слово цього списку.")
+print("Надрукувати початковий і змінений списки.")
+
+first_line = 'почему снова такое сложное задание Муха'
